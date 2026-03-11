5c4eedd32cb2387ad1460184903b772dfabb1221
maatheusZK <maatheusseveriano@gmail.com>
2026-03-11 14:28:22 -0300

    Add files via upload

---
+++ UC - Algoritmo (11-03-2026)/questão1.py
@@ -0,0 +1,15 @@
+salarioBase = 3500.00
+bonus = 800.00
+desconto = 250.00
+
+salarioBruto = salarioBase + bonus
+salarioLiquido = salarioBruto - desconto
+
+print("Salário bruto: ", salarioBruto)
+print("Salário líquido: ", salarioLiquido)
+
+print("Tipo de salarioBase: ", type(salarioBase))
+print("Tipo de bonus: ", type(bonus))
+print("Tipo de desconto: ", type(desconto))
+print("Tipo de salarioBruto: ", type(salarioBruto))
+print("Tipo de salarioLiquido: ", type(salarioLiquido))
