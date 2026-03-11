38703dd6aeb2d222e72d02e47bb8823fd09cc3a7
maatheusZK <maatheusseveriano@gmail.com>
2026-03-11 11:23:19 -0300

    Add files via upload

---
+++ UC - Algoritmo (11-03-2026)/questão2.py
@@ -0,0 +1,11 @@
+distancia = 450
+consumoCarro = 8
+precoLitro = 5.50
+
+litrosConsumidos = distancia / consumoCarro
+custoTotal = litrosConsumidos * precoLitro
+
+print("Distância percorrida: ", distancia, "km")
+print("Consumo do carro: ", consumoCarro, "km/l")
+print("Litros consumidos: ", litrosConsumidos)
+print("Custo total de combustível: R$", custoTotal)
---
+++ UC - Algoritmo (11-03-2026)/questão3.py
@@ -0,0 +1,6 @@
+fahrenheit = 32
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("Temperatura em fahrenheit: ", fahrenheit)
+print("Temperatura em celsius: ", celsius)
